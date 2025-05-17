80364b8e4edaff7443c713aa5b67ee42cd9fc33f
jcuervonarvaez <jcuervonarvaez@outlook.com>
2025-05-17 19:15:09 -0400

    updadte

---
+++ chamber/scripts/shared.js
@@ -0,0 +1,18 @@
+const buttonMenuMobile = document.getElementById("menu-mobile");
+const buttonMenuMobileClose = document.getElementById("menu-mobile-close");
+const navElement = document.getElementById("navMenu");
+const today = new Date();
+
+const currentDateElement = document.getElementById("currentYear");
+const lastUpdatedElement = document.getElementById("lastModified");
+
+currentDateElement.innerHTML = today.getFullYear();
+lastUpdatedElement.innerHTML = `Last Modification: ${document.lastModified}`;
+
+buttonMenuMobile.addEventListener("click", function () {
+  navElement.classList.toggle("active");
+});
+
+buttonMenuMobileClose.addEventListener("click", function () {
+  navElement.classList.toggle("active");
+});
