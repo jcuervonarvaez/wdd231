2aa336a79b677156e653efb7886c1e2c816ea235
jcuervonarvaez <jcuervonarvaez@outlook.com>
2025-05-17 22:52:18 -0400

    updadte

---
+++ chamber/scripts/bussiness-directory.js
@@ -0,0 +1,196 @@
+const bussinessData = [
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+  {
+    name: "Librería Nube de Tinta",
+    address: "Calle Esmeralda 526, Valparaíso, Chile",
+    phone: "+56 32 224 5678",
+    website: "http://nubedetinta.cl",
+    image: "nubedetinta_logo.png",
+    membership_level: 1,
+    category: "Librería & Cultura",
+    description:
+      "Librería independiente con énfasis en autores locales y actividades culturales semanales.",
+    social_media: {
+      facebook: "https://facebook.com/nubedetintavalpo",
+    },
+  },
+];
+
+const cardsGridElement = document.getElementById("business-cards");
+
+function addGridCard(busineess) {
+  let cardWrapper = document.createElement("div");
+  let cardTitle = document.createElement("h2");
+  let cardLogo = document.createElement("img")
+
+  cardTitle.textContent = `${busineess.name}`;
+  cardLogo.setAttribute('src', 'images/logo-dark.webp');
+  cardLogo.setAttribute('alt', `logo of ${busineess.name}`);
+  cardLogo.setAttribute('title', `logo of ${busineess.name}`);
+  cardLogo.setAttribute('width', '300');
+  cardLogo.setAttribute('heigth', '300');
+
+
+  cardWrapper.classList.add("card");
+  cardWrapper.appendChild(cardLogo);
+  cardWrapper.appendChild(cardTitle);
+
+  cardsGridElement.appendChild(cardWrapper);
+}
+
+bussinessData.forEach((busineess) => {
+  addGridCard(busineess);
+});
